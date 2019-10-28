d54d1245442aa7c8051415dcdc8f274d338686d7
student <student@Sanntid.(none)>
2019-10-28 14:25:13 +0100

    finished the labgit statusgit status

--- Ex8/main.c
+++ Ex8/main.c
@@ -19,6 +19,17 @@ int set_cpu(int cpu_number){
   return pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpu);
 }
 
+typedef struct Resource {
+  RT_MUTEX*  mutex;
+  int       priority;
+} Resource;
+
+typedef struct ThreadPriorities {
+  RT_TASK*   task;
+  int       base;
+  int       boost;
+} ThreadPriorities;
+
 RT_TASK periodic_RT_task;
 RT_TASK taskA_RT_thread_1;
 RT_TASK taskA_RT_thread_2;
@@ -30,6 +41,10 @@ RT_MUTEX mutex;
 RT_MUTEX mutexA;
 RT_MUTEX mutexB;
 RT_SEM semaphore;
+struct Resource resourceA = { .mutex = &mutexA, .priority = 95};
+struct Resource resourceB = { .mutex = &mutexB, .priority = 96};
+struct ThreadPriorities thread1_str = { .task = &taskD_RT_thread_1, .base = 70};
+struct ThreadPriorities thread2_str = { .task = &taskD_RT_thread_2, .base = 80};
 
 void busy_wait_us(unsigned long delay){
   for(; delay > 0; delay--){
@@ -37,6 +52,16 @@ void busy_wait_us(unsigned long delay){
   }
 }
 
+void icpp_lock(Resource *resc, ThreadPriorities *task_str){
+  rt_task_set_priority(task_str->task, resc->priority);
+  rt_mutex_acquire(resc->mutex, TM_INFINITE);
+  task_str->boost = resc->priority;
+}
+
+void icpp_unlock(struct Resource *resc, struct ThreadPriorities *task_str){
+  rt_task_set_priority(task_str->task,task_str->base);
+  rt_mutex_release(resc->mutex);
+}
 
 void* periodic_task(){
 	RTIME tstart = rt_timer_read();
@@ -108,24 +133,28 @@ void* taskD_thread_1(void* arg) {
 
 //	rt_task_wait_period(NULL);
   //Barrier synchronization
+  rt_printf("Task1: starting semaphore\n\r");
   rt_sem_p(&semaphore,TM_INFINITE);
-  rt_sem_v(&semaphore);
   rt_printf("Task1: Waiting and releasing semaphore\n\r");
 
   // Low priority task that locks the resource
-  rt_mutex_acquire(&mutexA,TM_INFINITE);
+  icpp_lock(&resourceA, &thread1_str);
   rt_printf("Task1 locking mutexA\n");
+
   rt_printf("Task1 busy wait start\n");
   busy_wait_us(300000);
   rt_printf("Task1 busy wait stopped\n");
-  rt_mutex_acquire(&mutexB,TM_INFINITE);
+
+  icpp_lock(&resourceB, &thread1_str);
   rt_printf("Task1 locking mutexB\n");
   rt_printf("Task1 busy wait start\n");
   busy_wait_us(300000);
   rt_printf("Task1 busy wait stopped\n");
-  rt_mutex_release(&mutexB);
+
+  icpp_unlock(&resourceB, &thread1_str);
   rt_printf("Task1 releasing mutexB\n");
-  rt_mutex_release(&mutexA);
+
+  icpp_unlock(&resourceA, &thread1_str);
   rt_printf("Task1 releasing mutexA\n");
   busy_wait_us(100000);
 }
@@ -138,24 +167,28 @@ void* taskD_thread_2(void* arg) {
 //	rt_task_wait_period(NULL);
   //Barrier synchronization
   rt_sem_p(&semaphore,TM_INFINITE);
-  rt_sem_v(&semaphore);
-  rt_printf("Task1: Waiting and releasing semaphore\n\r");
+  rt_printf("Task2: Waiting and releasing semaphore\n\r");
 
   // Low priority task that locks the resource
   rt_task_sleep(100000000);
-  rt_mutex_acquire(&mutexB,TM_INFINITE);
+  icpp_lock(&resourceB, &thread2_str);
   rt_printf("Task2 locking mutexB\n");
+
   rt_printf("Task2 busy wait start\n");
   busy_wait_us(100000);
   rt_printf("Task2 busy wait stopped\n");
-  rt_mutex_acquire(&mutexA,TM_INFINITE);
+
+  icpp_lock(&resourceA, &thread2_str);
   rt_printf("Task2 locking mutexA\n");
+
   rt_printf("Task2 busy wait start\n");
   busy_wait_us(200000);
   rt_printf("Task2 busy wait stopped\n");
-  rt_mutex_release(&mutexA);
+
+  icpp_unlock(&resourceA, &thread2_str);
   rt_printf("Task2 releasing mutexA\n");
-  rt_mutex_release(&mutexB);
+
+  icpp_unlock(&resourceB, &thread2_str);
   rt_printf("Task2 releasing mutexB\n");
   busy_wait_us(100000);
 }
@@ -213,9 +246,13 @@ int main(){
 	//rt_task_create(&taskA_RT_thread_1, "task1", 0, 70, T_CPU(0));
 	//rt_task_create(&taskA_RT_thread_2, "task2", 0, 75, T_CPU(0));
 	//rt_task_create(&taskA_RT_thread_3, "task3", 0, 80, T_CPU(0));
-	//rt_task_create(&taskA_RT_thread_4, "task4", 0, 90, T_CPU(0));
-	rt_task_create(&taskD_RT_thread_1, "task3", 0, 80, T_CPU(0));
-	rt_task_create(&taskD_RT_thread_2, "task4", 0, 90, T_CPU(0));
+	rt_task_create(&taskA_RT_thread_4, "task4", 0, 99, T_CPU(0));
+	//rt_task_create(&taskD_RT_thread_1, "task3", 0, 80, T_CPU(0));
+	//rt_task_create(&taskD_RT_thread_2, "task4", 0, 90, T_CPU(0));
+//	rt_task_create(&taskD_RT_thread_1, "task3", 0, thread1_str.base, T_CPU(0));
+//	rt_task_create(&taskD_RT_thread_2, "task3", 0, thread2_str.base, T_CPU(0));
+	rt_task_create(&thread1_str.task, "task1", 0, thread1_str.base, T_CPU(0));
+	rt_task_create(&thread2_str.task, "task2", 0, thread2_str.base, T_CPU(0));
 	
 //	rt_task_set_periodic(&taskA_RT_thread_1, TM_NOW, (1 * 1000 * 1000));
 //	rt_task_set_periodic(&taskA_RT_thread_2, TM_NOW, (1 * 1000 * 1000));
@@ -224,9 +261,9 @@ int main(){
 	//rt_task_start(&taskA_RT_thread_1, &taskA_thread_1, NULL);
 	//rt_task_start(&taskA_RT_thread_2, &taskA_thread_2, NULL);
 	//rt_task_start(&taskA_RT_thread_3, &taskA_thread_3, NULL);
-	//rt_task_start(&taskA_RT_thread_4, &taskA_thread_4, NULL);
-	rt_task_start(&taskD_RT_thread_1, &taskD_thread_1, NULL);
-	rt_task_start(&taskD_RT_thread_2, &taskD_thread_2, NULL);
+	rt_task_start(&taskA_RT_thread_4, &taskA_thread_4, NULL);
+	rt_task_start(&thread1_str.task, &taskD_thread_1, NULL);
+	rt_task_start(&thread2_str.task, &taskD_thread_2, NULL);
 	while(1){};
 	/*
   	for (int i = 0; i < 10; i++){
