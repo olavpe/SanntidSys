10c6520548a25311b77ba348dc69c3d283da4db8
student <student@Sanntid.(none)>
2019-10-28 12:28:46 +0100

    finished task D

--- Ex8/main.c
+++ Ex8/main.c
@@ -24,7 +24,11 @@ RT_TASK taskA_RT_thread_1;
 RT_TASK taskA_RT_thread_2;
 RT_TASK taskA_RT_thread_3;
 RT_TASK taskA_RT_thread_4;
+RT_TASK taskD_RT_thread_1;
+RT_TASK taskD_RT_thread_2;
 RT_MUTEX mutex;
+RT_MUTEX mutexA;
+RT_MUTEX mutexB;
 RT_SEM semaphore;
 
 void busy_wait_us(unsigned long delay){
@@ -97,6 +101,64 @@ void* taskA_thread_3(void* arg) {
   rt_printf("Task3 busy wait stopped\n");
 }
 
+void* taskD_thread_1(void* arg) {
+  set_cpu(T_CPU(0));
+  unsigned long duration = 30000000000; // 30 second timeout
+  unsigned long endTime = rt_timer_read() + duration;
+
+//	rt_task_wait_period(NULL);
+  //Barrier synchronization
+  rt_sem_p(&semaphore,TM_INFINITE);
+  rt_sem_v(&semaphore);
+  rt_printf("Task1: Waiting and releasing semaphore\n\r");
+
+  // Low priority task that locks the resource
+  rt_mutex_acquire(&mutexA,TM_INFINITE);
+  rt_printf("Task1 locking mutexA\n");
+  rt_printf("Task1 busy wait start\n");
+  busy_wait_us(300000);
+  rt_printf("Task1 busy wait stopped\n");
+  rt_mutex_acquire(&mutexB,TM_INFINITE);
+  rt_printf("Task1 locking mutexB\n");
+  rt_printf("Task1 busy wait start\n");
+  busy_wait_us(300000);
+  rt_printf("Task1 busy wait stopped\n");
+  rt_mutex_release(&mutexB);
+  rt_printf("Task1 releasing mutexB\n");
+  rt_mutex_release(&mutexA);
+  rt_printf("Task1 releasing mutexA\n");
+  busy_wait_us(100000);
+}
+
+void* taskD_thread_2(void* arg) {
+  set_cpu(T_CPU(0));
+  unsigned long duration = 30000000000; // 30 second timeout
+  unsigned long endTime = rt_timer_read() + duration;
+
+//	rt_task_wait_period(NULL);
+  //Barrier synchronization
+  rt_sem_p(&semaphore,TM_INFINITE);
+  rt_sem_v(&semaphore);
+  rt_printf("Task1: Waiting and releasing semaphore\n\r");
+
+  // Low priority task that locks the resource
+  rt_task_sleep(100000000);
+  rt_mutex_acquire(&mutexB,TM_INFINITE);
+  rt_printf("Task2 locking mutexB\n");
+  rt_printf("Task2 busy wait start\n");
+  busy_wait_us(100000);
+  rt_printf("Task2 busy wait stopped\n");
+  rt_mutex_acquire(&mutexA,TM_INFINITE);
+  rt_printf("Task2 locking mutexA\n");
+  rt_printf("Task2 busy wait start\n");
+  busy_wait_us(200000);
+  rt_printf("Task2 busy wait stopped\n");
+  rt_mutex_release(&mutexA);
+  rt_printf("Task2 releasing mutexA\n");
+  rt_mutex_release(&mutexB);
+  rt_printf("Task2 releasing mutexB\n");
+  busy_wait_us(100000);
+}
 void* taskA_thread_4(void* arg) {
   set_cpu(T_CPU(0));
   unsigned long duration = 30000000000; // 30 second timeout
@@ -113,6 +175,8 @@ void* taskA_thread_4(void* arg) {
   rt_task_sleep(500000000000);
   rt_sem_delete(&semaphore);
   rt_mutex_delete(&mutex);
+  rt_mutex_delete(&mutexA);
+  rt_mutex_delete(&mutexB);
 }
 
 void* disturbance_thread(void* arg){
@@ -127,8 +191,12 @@ int main(){
 	rt_print_auto_init(1);
 	int err;
 	int err_mut;
+	int err_mutA;
+	int err_mutB;
 	err = rt_sem_create(&semaphore,"MySempahore", 1, S_FIFO);
 	err_mut = rt_mutex_create(&mutex,"MyMutex");
+	err_mutA = rt_mutex_create(&mutexA,"MyMutexA");
+	err_mutB = rt_mutex_create(&mutexB,"MyMutexB");
 
 	/*
 	pthread_t disturbance[10];
@@ -142,19 +210,23 @@ int main(){
 	//rt_task_set_periodic(&periodic_RT_task, TM_NOW, (1 * 1000 * 1000));
 	//rt_task_start(&periodic_RT_task, &periodic_task, NULL);
 	
-	rt_task_create(&taskA_RT_thread_1, "task1", 0, 70, T_CPU(0));
-	rt_task_create(&taskA_RT_thread_2, "task2", 0, 75, T_CPU(0));
-	rt_task_create(&taskA_RT_thread_3, "task3", 0, 80, T_CPU(0));
-	rt_task_create(&taskA_RT_thread_4, "task4", 0, 90, T_CPU(0));
+	//rt_task_create(&taskA_RT_thread_1, "task1", 0, 70, T_CPU(0));
+	//rt_task_create(&taskA_RT_thread_2, "task2", 0, 75, T_CPU(0));
+	//rt_task_create(&taskA_RT_thread_3, "task3", 0, 80, T_CPU(0));
+	//rt_task_create(&taskA_RT_thread_4, "task4", 0, 90, T_CPU(0));
+	rt_task_create(&taskD_RT_thread_1, "task3", 0, 80, T_CPU(0));
+	rt_task_create(&taskD_RT_thread_2, "task4", 0, 90, T_CPU(0));
 	
 //	rt_task_set_periodic(&taskA_RT_thread_1, TM_NOW, (1 * 1000 * 1000));
 //	rt_task_set_periodic(&taskA_RT_thread_2, TM_NOW, (1 * 1000 * 1000));
 //	rt_task_set_periodic(&taskA_RT_thread_3, TM_NOW, (1 * 1000 * 1000));
 	
-	rt_task_start(&taskA_RT_thread_1, &taskA_thread_1, NULL);
-	rt_task_start(&taskA_RT_thread_2, &taskA_thread_2, NULL);
-	rt_task_start(&taskA_RT_thread_3, &taskA_thread_3, NULL);
-	rt_task_start(&taskA_RT_thread_4, &taskA_thread_4, NULL);
+	//rt_task_start(&taskA_RT_thread_1, &taskA_thread_1, NULL);
+	//rt_task_start(&taskA_RT_thread_2, &taskA_thread_2, NULL);
+	//rt_task_start(&taskA_RT_thread_3, &taskA_thread_3, NULL);
+	//rt_task_start(&taskA_RT_thread_4, &taskA_thread_4, NULL);
+	rt_task_start(&taskD_RT_thread_1, &taskD_thread_1, NULL);
+	rt_task_start(&taskD_RT_thread_2, &taskD_thread_2, NULL);
 	while(1){};
 	/*
   	for (int i = 0; i < 10; i++){
